436fe2fb008a0798cead77241bda4c58977b8984
github username <github email address>
2019-04-13 14:55:40 -0400

    its a danged logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
             MAHALO
+            <Home />
       </div>
     );
   }
