9797b5633723198ac8f72b873a350688bad8826c
mwestervelt <elle.westervelt@gmail.com>
2019-04-13 17:41:14 -0400

    question toggle

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import logo_white from './mahalo_logo_white.svg'
+import QuestionContainer from './QuestionContainer'
+
 
 export default class Home extends Component {
+
+  state = {
+    showQs: false
+  }
+
+  toggleQuestions = () => {
+    this.setState({ showQs: !this.state.showQs })
+  }
+
   render(){
+    console.log(this.state)
     return(
       <div className='logo-div'>
         <img className='logo' src={ logo_white } alt='logo' />
-      
+        {this.state.showQs ?
+          <QuestionContainer
+            choice={this.props.choice}
+            risklevel={this.props.risklevel}
+            handleChange={this.props.handleChange}
+            />
+          :
+          <button onClick={this.toggleQuestions}>Start!</button>
+        }
       </div>
 
     )
